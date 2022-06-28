fc9d5130283ae37bbda168fd548257df346e4c50
Ksenia <k.g.lebedeva@gmail.com>
2022-06-28 23:19:03 +0300

    feat: reverse task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    n = Math.abs(n);
+  let string = n.toString();
+  let newString = '';
+
+  for (let i = 0; i < string.length; i++) {
+    newString = string[i] + newString;
+  }
+  return newString - 0;
 }
